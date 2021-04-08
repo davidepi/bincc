9127e571c1efa6a64bb966a177b0d6f970a3acdc
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2021-04-08 16:40:46 +0900

    doc: CFG basic documentation
    Still missing examples, because building a CFG without using external
    files is tedious. Probabily I should add a very basic sample CFG.

--- src/analysis/cfg.rs
+++ src/analysis/cfg.rs
@@ -9,19 +9,34 @@ use std::io::Write;
 use std::ops::Index;
 use std::path::Path;
 
+/// A Control Flow Graph.
+///
+/// Struct representing a Control Flow Graph (CFG).
+/// This is a graph representation of all the possible execution paths in a function.
 #[derive(Debug, Clone)]
 pub struct CFG {
     nodes: Vec<BasicBlock>,
     edges: Vec<[Option<usize>; 2]>,
 }
 
+/// Minimum portion of code without any jump.
+///
+/// Represents a list of statements without any jump, except for the last one.
+/// This does not guarantee, however, that the last statement will be a jump.
+/// For example, the [CFG::new()] method generates basic blocks in such a way that each jump inside
+/// the CFG lands exactly in the first instruction of each basic block (instead of, for example,
+/// in the middle of it). This creates some blocks without any jumps inside them but also not
+/// terminating with a jump.
+///
+/// This class does not contains the actual statements, rather than their offsets in the original
+/// code.
 #[derive(Debug, Clone)]
 pub struct BasicBlock {
-    // id of bb
+    /// Numerical integer representing an unique identifier for this block.
     pub id: usize,
-    // offset of first instruction
+    /// Offset in the original code where this basic block begins.
     pub first: u64,
-    // offset of last instruction (beginning of the instruction)
+    /// Offset in the original code where this basic block ends.
     pub last: u64,
 }
 
@@ -41,6 +56,7 @@ impl Display for CFG {
     }
 }
 
+/// An iterator for contents of a CFG.
 pub struct CFGIter<'a> {
     stack: Vec<&'a BasicBlock>,
 }
@@ -54,10 +70,41 @@ impl<'a> Iterator for CFGIter<'a> {
 }
 
 impl CFG {
+    /// Creates a new CFG from a list of statements.
+    ///
+    /// Given a list of statements and a source architectures, builds the CFG for that list.
+    /// The list of statements is presented as slice.
+    ///
+    /// The newly returned CFG will not contain a sink and may present unreachable nodes (mainly due
+    /// to indirect jumps). One should use [CFG::add_sink()] or [CFG::reachable()] to refine the
+    /// CFG.
+    /// # Examples
+    /// Basic usage:
+    /// ```
+    /// use bcc::analysis::CFG;
+    /// use bcc::disasm::{ArchX86, Statement};
+    ///
+    /// let stmts = vec![
+    ///     Statement::new(0x38, "cmp dword [var_4h], 0"),
+    ///     Statement::new(0x3C, "jle 0x45"),
+    ///     Statement::new(0x3E, "mov eax, 0"),
+    ///     Statement::new(0x43, "jmp 0x4a"),
+    ///     Statement::new(0x45, "mov eax, 1"),
+    ///     Statement::new(0x4A, "ret"),
+    /// ];
+    /// let arch = ArchX86::new_amd64();
+    /// let cfg = CFG::new(&stmts, &arch);
+    ///
+    /// assert_eq!(cfg.len(), 4);
+    /// ```
     pub fn new(stmts: &[Statement], arch: &dyn Architecture) -> CFG {
         build_cfg(stmts, arch)
     }
 
+    /// Visits the CFG nodes in pre-order.
+    ///
+    /// Returns an iterator visiting every node reachable from the CFG root using a depth-first
+    /// pre-order.
     pub fn preorder(&self) -> CFGIter {
         if !self.is_empty() {
             let mut buffer = vec![0];
@@ -84,6 +131,10 @@ impl CFG {
         }
     }
 
+    /// Visits the CFG nodes in post-order.
+    ///
+    /// Returns an iterator visiting every node reachable from the CFG root using a depth-first
+    /// post-order.
     pub fn postorder(&self) -> CFGIter {
         if !self.is_empty() {
             let mut buffer = vec![0];
@@ -115,22 +166,36 @@ impl CFG {
         }
     }
 
+    /// Returns the next basic block.
+    ///
+    /// Given a basic block id, returns the basic block id of its follower.
+    /// This means the target of an unconditional jump or the next block in case the current block
+    /// ends with a conditional jump.
+    ///
+    /// Returns None if there is no next block.
     pub fn next(&self, id: usize) -> Option<usize> {
         self.edges[id][0]
     }
 
+    /// Returns the conditional basic block.
+    ///
+    /// Given a basic block id, returns the basic block id of the conditional jump target.
+    /// If the current block does not end with a conditional jump, None is returned.
     pub fn cond(&self, id: usize) -> Option<usize> {
         self.edges[id][1]
     }
 
+    /// Returns the number of blocks in this CFG.
     pub fn len(&self) -> usize {
         self.nodes.len()
     }
 
+    /// Returns true if the current CFG is empty.
     pub fn is_empty(&self) -> bool {
         self.nodes.is_empty()
     }
 
+    /// Converts the current CFG into a Graphviz dot representation.
     pub fn to_dot(&self) -> String {
         let mut content = Vec::new();
         let sink_iter = self
@@ -161,11 +226,20 @@ impl CFG {
         format!("digraph {{\n{}\n}}\n", content.join("\n"))
     }
 
+    /// Saves the current CFG into a Graphviz representation.
+    ///
+    /// Given a path to file, saves the current CFG as a Graphviz .dot file.
+    /// This is equivalent of calling [CFG::to_dot()] and then saving the String content to file.
     pub fn to_file<S: AsRef<Path>>(&self, filename: S) -> Result<(), io::Error> {
         let mut file = File::create(filename)?;
         file.write_all(self.to_dot().as_bytes())
     }
 
+    /// Adds a sink to the current CFG.
+    ///
+    /// In some cases, a CFG may have multiple nodes without children (like in the case of multiple
+    /// return statements). This method merges those nodes by attaching them to a sink. The sink
+    /// is recognizable by having both starting and ending offsets equal to 0.
     pub fn add_sink(&self) -> CFG {
         let exit_nodes = self
             .edges
@@ -197,10 +271,14 @@ impl CFG {
         CFG { nodes, edges }
     }
 
+    /// Removes unreachable nodes.
+    ///
+    /// Removes nodes that are not reachable from the CFG root by any path. These nodes are usually
+    /// created when there are indirect jumps in the original statement list.
     pub fn reachable(&self) -> CFG {
         if self.nodes.len() > 1 {
             let mut new_nodes = vec![self.nodes[0].clone()];
-            let mut new_edges = vec![self.edges[0].clone()];
+            let mut new_edges = vec![self.edges[0]];
             let reachables = self
                 .preorder()
                 .enumerate()
@@ -211,7 +289,7 @@ impl CFG {
                 skipped[index] = skipped[index - 1];
                 if reachables.contains(&index) {
                     new_nodes.push(self.nodes[index].clone());
-                    new_edges.push(self.edges[index].clone());
+                    new_edges.push(self.edges[index]);
                 } else {
                     skipped[index] += 1;
                 }
@@ -242,14 +320,21 @@ impl Index<usize> for CFG {
     }
 }
 
+// struct containing multiple maps related to jumps sources/dests
 struct TargetMap {
+    // list of offsets that ends up being targets for a jump somewhere
     targets: BTreeSet<u64>,
+    // map for conditional jumps: <source offset, dest offset>
     srcs_cond: FnvHashMap<u64, u64>,
+    // map for unconditional jumps: <source offset, dest offset>
     srcs_uncond: FnvHashMap<u64, u64>,
+    // set for conditional returns containing the return offset
     deadend_cond: BTreeSet<u64>,
+    // set for unconditional returns containing the return offset
     deadend_uncond: BTreeSet<u64>,
 }
 
+// given a list of Statements and an Architecture creates the TargetMap struct
 fn get_targets(stmts: &[Statement], arch: &dyn Architecture) -> TargetMap {
     let mut targets = BTreeSet::default();
     let mut srcs_cond = FnvHashMap::default();
@@ -318,6 +403,13 @@ fn get_targets(stmts: &[Statement], arch: &dyn Architecture) -> TargetMap {
     }
 }
 
+// given an offset (any offset) returns the corresponding basic block id containing it.
+// requires:
+// - the actual offset
+// - a map <basic block starting offset, basic block id>
+// - list of jump targets
+// this method is used ad-hoc inside the build_cfg function and needs to be rewritten to be used in
+// any other case
 fn resolve_bb_id(offset: u64, id_map: &FnvHashMap<u64, usize>, targets: &BTreeSet<u64>) -> usize {
     if let Some(ret) = id_map.get(&offset) {
         // corner case: the current offset is also the block start
@@ -329,6 +421,7 @@ fn resolve_bb_id(offset: u64, id_map: &FnvHashMap<u64, usize>, targets: &BTreeSe
     }
 }
 
+// actual cfg building
 fn build_cfg(stmts: &[Statement], arch: &dyn Architecture) -> CFG {
     let all_offsets = stmts
         .iter()
