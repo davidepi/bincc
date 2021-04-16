66c2903e9b0bf30685f1ce771109d25700f09af2
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2021-04-16 12:48:44 +0900

    refactor: remove id from AbstractBlock
    It is not necessary (not yet at least) and just creates confusion with
    the graph indexing.

--- src/analysis/blocks.rs
+++ src/analysis/blocks.rs
@@ -13,7 +13,6 @@ pub enum BlockType {
 }
 
 pub trait AbstractBlock<H: Hasher> {
-    fn get_id(&self) -> usize;
     fn get_type(&self) -> BlockType;
     fn get_depth(&self) -> u32;
     fn children(&self) -> &[Box<dyn AbstractBlock<H>>];
@@ -35,10 +34,6 @@ pub trait AbstractBlock<H: Hasher> {
 }
 
 impl<H: Hasher> AbstractBlock<H> for BasicBlock {
-    fn get_id(&self) -> usize {
-        self.id
-    }
-
     fn get_type(&self) -> BlockType {
         BlockType::Basic
     }
@@ -53,30 +48,24 @@ impl<H: Hasher> AbstractBlock<H> for BasicBlock {
 }
 
 pub struct StructureBlock<H: Hasher> {
-    id: usize,
     block_type: BlockType,
     content: Vec<Box<dyn AbstractBlock<H>>>,
     depth: u32,
 }
 
 impl<H: Hasher> StructureBlock<H> {
-    pub fn new_self_loop(id: usize, child: Box<dyn AbstractBlock<H>>) -> Box<StructureBlock<H>> {
+    pub fn new_self_loop(child: Box<dyn AbstractBlock<H>>) -> Box<StructureBlock<H>> {
         let old_depth = child.get_depth();
         Box::new(StructureBlock {
-            id,
             block_type: BlockType::SelfLooping,
             content: vec![child],
             depth: old_depth + 1,
         })
     }
 
-    pub fn new_sequence(
-        id: usize,
-        children: Vec<Box<dyn AbstractBlock<H>>>,
-    ) -> Box<StructureBlock<H>> {
+    pub fn new_sequence(children: Vec<Box<dyn AbstractBlock<H>>>) -> Box<StructureBlock<H>> {
         let old_depth = children.iter().fold(0, |max, val| max.max(val.get_depth()));
         Box::new(StructureBlock {
-            id,
             block_type: BlockType::Sequence,
             content: children,
             depth: old_depth + 1,
@@ -84,34 +73,28 @@ impl<H: Hasher> StructureBlock<H> {
     }
 
     pub fn new_if_then(
-        id: usize,
         ifb: Box<dyn AbstractBlock<H>>,
         thenb: Box<dyn AbstractBlock<H>>,
     ) -> Box<StructureBlock<H>> {
         let children = vec![ifb, thenb];
-        let mut block = Self::new_sequence(id, children);
+        let mut block = Self::new_sequence(children);
         block.block_type = BlockType::IfThenElse;
         block
     }
 
     pub fn new_if_then_else(
-        id: usize,
         ifb: Box<dyn AbstractBlock<H>>,
         thenb: Box<dyn AbstractBlock<H>>,
         elseb: Box<dyn AbstractBlock<H>>,
     ) -> Box<StructureBlock<H>> {
         let children = vec![ifb, thenb, elseb];
-        let mut block = Self::new_sequence(id, children);
+        let mut block = Self::new_sequence(children);
         block.block_type = BlockType::IfThenElse;
         block
     }
 }
 
 impl<H: Hasher> AbstractBlock<H> for StructureBlock<H> {
-    fn get_id(&self) -> usize {
-        self.id
-    }
-
     fn get_type(&self) -> BlockType {
         self.block_type
     }
@@ -164,10 +147,10 @@ mod tests {
             first: 0,
             last: 0,
         });
-        let self_loop = StructureBlock::new_self_loop(1, bb.clone());
-        let sequence0 = StructureBlock::new_sequence(2, vec![self_loop, bb.clone()]);
-        let self_loop = StructureBlock::new_self_loop(3, bb.clone());
-        let sequence1 = StructureBlock::new_sequence(4, vec![self_loop, bb]);
+        let self_loop = StructureBlock::new_self_loop(bb.clone());
+        let sequence0 = StructureBlock::new_sequence(vec![self_loop, bb.clone()]);
+        let self_loop = StructureBlock::new_self_loop(bb.clone());
+        let sequence1 = StructureBlock::new_sequence(vec![self_loop, bb]);
         let hashes = calculate_hashes(*sequence0, *sequence1);
         assert_eq!(hashes.0, hashes.1)
     }
@@ -179,10 +162,10 @@ mod tests {
             first: 0,
             last: 0,
         });
-        let self_loop = StructureBlock::new_self_loop(1, bb.clone());
-        let sequence0 = StructureBlock::new_sequence(2, vec![self_loop, bb.clone()]);
-        let self_loop = StructureBlock::new_self_loop(3, bb.clone());
-        let sequence1 = StructureBlock::new_sequence(4, vec![bb, self_loop]);
+        let self_loop = StructureBlock::new_self_loop(bb.clone());
+        let sequence0 = StructureBlock::new_sequence(vec![self_loop, bb.clone()]);
+        let self_loop = StructureBlock::new_self_loop(bb.clone());
+        let sequence1 = StructureBlock::new_sequence(vec![bb, self_loop]);
         let hashes = calculate_hashes(*sequence0, *sequence1);
         assert_ne!(hashes.0, hashes.1)
     }
