6ad3bb241e1683551e4fb08fd6f05490e0cbeb8c
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2021-05-24 13:42:59 +0900

    feat: add logging for main
    Fixed also the non-working portion.

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,14 @@ use std::path::Path;
 use std::process::exit;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::Instant;
 
 fn main() {
+    match std::env::var("RUST_LOG") {
+        Ok(_) => {}
+        Err(_) => std::env::set_var("RUST_LOG", "warn"),
+    }
+    env_logger::init();
     let matches = App::new("bcc")
         .version("0.1")
         .author("Davide Pizzolotto <davide.pizzolotto@gmail.com>")
@@ -32,8 +38,35 @@ fn main() {
                 .required(true)
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("jobs")
+                .short("j")
+                .long("jobs")
+                .help("Number of working jobs")
+                .required(false)
+                .takes_value(true),
+        )
         .get_matches();
-
+    let threads_no = if cfg!(debug_assertions) {
+        log::warn!("Debug build, forcing 1 working thread");
+        1
+    } else if let Some(jobs) = matches.value_of("jobs") {
+        match jobs.parse::<usize>() {
+            Ok(val) => val,
+            Err(_) => {
+                let default_threads = num_cpus::get();
+                log::warn!(
+                    "Failed to parse jobs number, defaulting to {}",
+                    default_threads
+                );
+                default_threads
+            }
+        }
+    } else {
+        let default_threads = num_cpus::get();
+        log::info!("Using {} jobs", default_threads);
+        default_threads
+    };
     let output = Path::new(matches.value_of("output").unwrap());
     if !output.exists() {
         log::error!("Output folder {} does not exist!", output.to_str().unwrap());
@@ -44,19 +77,25 @@ fn main() {
         .unwrap()
         .map(String::from)
         .collect::<Vec<_>>();
+    log::debug!("Total jobs: {}", inputs.len());
     let jobs = Arc::new(Mutex::new(inputs));
     let mut threads = vec![];
-    for _ in 1..num_cpus::get() {
+    for t in 0..threads_no {
         threads.push(thread::spawn({
             let personal_jobs = jobs.clone();
-            let out = output.clone().to_str().unwrap();
+            let out = output.clone().to_str().unwrap().to_string();
             move || loop {
                 if !personal_jobs.lock().unwrap().is_empty() {
                     let maybe_job = personal_jobs.lock().unwrap().pop();
                     if let Some(job) = maybe_job {
-                        match get_and_save_cfg(&job, out) {
-                            Ok(_) => {}
-                            Err(err) => log::warn!("Could not process file {}: {}", job, err),
+                        log::trace!("[{}] starting job on {}", t, job);
+                        match get_and_save_cfg(&job, &out, t) {
+                            Ok(_) => {
+                                log::trace!("[{}] finished job on {}", t, job);
+                            }
+                            Err(err) => {
+                                log::warn!("[{}] could not process file {}: {}", t, job, err)
+                            }
                         }
                     }
                 } else {
@@ -70,21 +109,49 @@ fn main() {
     }
 }
 
-fn get_and_save_cfg(relative: &str, output_dir: &str) -> Result<Vec<CFG>, Box<dyn Error>> {
+fn get_and_save_cfg(
+    relative: &str,
+    output_dir: &str,
+    tid: usize,
+) -> Result<Vec<CFG>, Box<dyn Error>> {
     let relative_path = Path::new(relative);
     let filename = relative_path.file_name().unwrap();
     let out_dir = Path::new(output_dir).join(Path::new(filename));
-    std::fs::create_dir(out_dir)?;
-    let disassembler = R2Disasm::new(relative)?;
+    let metadata = std::fs::metadata(relative_path)?;
+    std::fs::create_dir(out_dir.clone())?;
+    log::info!(
+        "Created folder {}",
+        out_dir.as_os_str().to_str().unwrap_or("ERR")
+    );
+    let mut disassembler = R2Disasm::new(relative)?;
+    log::trace!("[{}] starting disassembling", tid);
+    let start_t = Instant::now();
+    disassembler.analyse();
+    let end_t = Instant::now();
+    log::trace!("[{}] finished disassembling", tid);
+    log::info!(
+        "[{}] disassembling {} ({} bytes) took {} ms",
+        tid,
+        relative,
+        metadata.len(),
+        end_t.checked_duration_since(start_t).unwrap().as_millis()
+    );
     let mut ret = Vec::new();
     if let Some(arch) = disassembler.get_arch() {
         let fnames = disassembler.get_function_names();
         let bodies = disassembler.get_function_bodies();
+        log::debug!(
+            "[{}] found {} function bodies for {}",
+            tid,
+            bodies.len(),
+            relative
+        );
         for (function, offset) in fnames {
             let graph_filename = format!("{}{}", function, ".dot");
-            let outfile = out_dir.join(Path::new(&graph_filename));
-            let cfg = CFG::new(&bodies.get(&offset).unwrap()[..], &arch);
-            cfg.to_file(outfile);
+            let outfile = out_dir.clone().join(Path::new(&graph_filename));
+            let cfg = CFG::new(&bodies.get(&offset).unwrap()[..], &*arch);
+            log::trace!("[{}] extracted CFG of {}::{}", tid, relative, function);
+            cfg.to_file(outfile)?;
             ret.push(cfg)
         }
         Ok(ret)
