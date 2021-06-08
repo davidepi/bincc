4fb249143480803cbc7c3602e6983d167f5b35c6
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2021-06-08 17:15:22 +0900

    feat: cfs generation step in the main

--- extractor/src/main.rs
+++ extractor/src/main.rs
@@ -1,11 +1,10 @@
-use bcc::analysis::CFG;
+use bcc::analysis::{CFG, CFS};
 use bcc::disasm::radare2::R2Disasm;
 use bcc::disasm::Disassembler;
 use clap::{App, Arg};
 use indicatif::{ProgressBar, ProgressStyle};
 use log::LevelFilter;
-use std::error::Error;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::process::exit;
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -24,9 +23,17 @@ fn main() {
                 .takes_value(true),
         )
         .arg(
+            Arg::with_name("extract")
+                .long("extract")
+                .help("Perform the CFG extraction only")
+                .takes_value(false),
+        )
+        .arg(
             Arg::with_name("output")
                 .help("Output directory")
-                .required(true)
+                .short("o")
+                .long("out")
+                .required(false)
                 .takes_value(true),
         )
         .arg(
@@ -72,51 +79,65 @@ fn main() {
         log::info!("Using {} threads", default_threads);
         default_threads
     };
-    let output = Path::new(matches.value_of("output").unwrap());
-    if !output.exists() {
-        log::error!("Output folder {} does not exist!", output.to_str().unwrap());
-        exit(1);
-    }
     let inputs = matches
         .values_of("input")
         .unwrap()
         .map(String::from)
         .collect::<Vec<_>>();
-    log::debug!("Total jobs: {}", inputs.len());
-    extract_dot(inputs, output.to_str().unwrap().to_owned(), threads_no);
+    if matches.is_present("extract") {
+        if let Some(out) = matches.value_of("output") {
+            if !Path::new(out).exists() {
+                log::error!("Output folder {} does not exist!", out);
+                exit(1);
+            } else {
+                log::debug!("Total jobs: {}", inputs.len());
+                multithreaded_work(
+                    inputs,
+                    Some(out.to_string()),
+                    extract_cfg_to_dot,
+                    threads_no,
+                );
+            }
+        } else {
+            let msg = "Output dir is required with the --extract flag!";
+            log::error!("{}", msg);
+            exit(1);
+        }
+    } else {
+        let extracted = inputs
+            .into_iter()
+            .flat_map(read_dot_files)
+            .collect::<Vec<_>>();
+        log::debug!("Total jobs: {}", extracted.len());
+        multithreaded_work(extracted, None, calculate_comparison, threads_no);
+    }
 }
 
-fn extract_dot(binaries: Vec<String>, output_dir: String, threads_no: usize) -> Vec<PathBuf> {
-    println!("Extracting CFGs...");
-    let pb = Arc::new(ProgressBar::new(binaries.len() as u64));
+fn multithreaded_work(
+    input_jobs: Vec<String>,
+    output: Option<String>,
+    function: fn(&str, Option<&str>, usize),
+    threads_no: usize,
+) {
+    let pb = Arc::new(ProgressBar::new(input_jobs.len() as u64));
     pb.set_style(
         ProgressStyle::default_bar()
             .template("[{elapsed_precise}] [{bar:40.cyan/blue}] {pos:>7}/{len:7}")
             .progress_chars("#>-"),
     );
-    let jobs = Arc::new(Mutex::new(binaries));
-    let ret = Arc::new(Mutex::new(Vec::new()));
+    let jobs = Arc::new(Mutex::new(input_jobs));
     let mut threads = vec![];
     for t in 0..threads_no {
         let personal_jobs = jobs.clone();
-        let personal_ret = ret.clone();
         let personal_pb = pb.clone();
-        let personal_outdir = output_dir.clone();
+        let personal_outdir = output.clone();
         threads.push(thread::spawn({
             move || loop {
                 if !personal_jobs.lock().unwrap().is_empty() {
                     let maybe_job = personal_jobs.lock().unwrap().pop();
                     if let Some(job) = maybe_job {
                         log::trace!("[{}] starting job on {}", t, job);
-                        match get_and_save_cfg(&job, &personal_outdir, t) {
-                            Ok(path) => {
-                                personal_ret.lock().unwrap().push(path);
-                                log::trace!("[{}] finished job on {}", t, job);
-                            }
-                            Err(err) => {
-                                log::error!("[{}] could not process file {}: {}", t, job, err)
-                            }
-                        }
+                        function(&job, personal_outdir.as_deref(), threads_no);
                         personal_pb.inc(1);
                     }
                 } else {
@@ -129,25 +150,38 @@ fn extract_dot(binaries: Vec<String>, output_dir: String, threads_no: usize) -> 
         t.join().unwrap();
     }
     pb.finish();
-    let x = ret.lock().unwrap().clone();
-    x
 }
 
-fn get_and_save_cfg(
-    relative: &str,
-    output_dir: &str,
-    tid: usize,
-) -> Result<PathBuf, Box<dyn Error>> {
-    let relative_path = Path::new(relative);
+fn extract_cfg_to_dot(input: &str, output: Option<&str>, tid: usize) {
+    let relative_path = Path::new(input);
     let filename = relative_path.file_name().unwrap();
-    let out_dir = Path::new(output_dir).join(Path::new(filename));
-    let metadata = std::fs::metadata(relative_path)?;
-    std::fs::create_dir(out_dir.clone())?;
+    let out_dir = Path::new(output.unwrap()).join(Path::new(filename));
+    let metadata = match std::fs::metadata(relative_path) {
+        Ok(val) => val,
+        Err(err) => {
+            log::error!("[{}] {}", tid, err);
+            return;
+        }
+    };
+    if std::fs::create_dir(out_dir.clone()).is_err() {
+        log::error!(
+            "[{}] Could not create output directory {}",
+            tid,
+            output.unwrap()
+        );
+        return;
+    }
     log::info!(
         "Created folder {}",
         out_dir.as_os_str().to_str().unwrap_or("ERR")
     );
-    let mut disassembler = R2Disasm::new(relative)?;
+    let mut disassembler = match R2Disasm::new(input) {
+        Ok(disasm) => disasm,
+        Err(err) => {
+            log::error!("[{}] Disassembler error: {}", tid, err);
+            return;
+        }
+    };
     if let Some(arch) = disassembler.get_arch() {
         log::trace!("[{}] starting disassembling", tid);
         let start_t = Instant::now();
@@ -157,7 +191,7 @@ fn get_and_save_cfg(
         log::info!(
             "[{}] disassembling {} ({} bytes) took {} ms",
             tid,
-            relative,
+            input,
             metadata.len(),
             end_t.checked_duration_since(start_t).unwrap().as_millis()
         );
@@ -167,24 +201,67 @@ fn get_and_save_cfg(
             "[{}] found {} function bodies for {}",
             tid,
             bodies.len(),
-            relative
+            input
         );
         for (function, offset) in fnames {
             let graph_filename = format!("{}{}", function, ".dot");
             let outfile = out_dir.clone().join(Path::new(&graph_filename));
             if let Some(body) = bodies.get(&offset) {
                 let cfg = CFG::new(&body[..], &*arch);
-                log::trace!("[{}] extracted CFG of {}::{}", tid, relative, function);
+                log::trace!("[{}] extracted CFG of {}::{}", tid, input, function);
                 cfg.to_file(outfile).unwrap_or_else(|_| {
-                    log::error!("[{}] could not save CFG of {}::{}", tid, relative, function)
+                    log::error!("[{}] could not save CFG of {}::{}", tid, input, function)
                 });
             }
         }
-        Ok(out_dir)
     } else {
-        Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::InvalidInput,
-            "Unknown architecture".to_string(),
-        )))
+        log::error!("[{}] Unknown architecture of file {}", tid, input);
+    }
+}
+
+fn calculate_comparison(input: &str, _: Option<&str>, tid: usize) {
+    if let Ok(cfg) = CFG::from_file(input) {
+        let cfg_sinked = cfg.add_sink();
+        let start_t = Instant::now();
+        let cfs = CFS::new(&cfg_sinked);
+        let end_t = Instant::now();
+        if cfs.get_tree().is_some() {
+            let duration = end_t.checked_duration_since(start_t).unwrap().as_millis();
+            log::debug!(
+                "[{}] CFS successful for {} (took {} ms)",
+                tid,
+                input,
+                duration
+            );
+        } else {
+            log::debug!("[{}] CFS failed for {}", tid, input);
+        }
+    } else {
+        log::error!("[{}] Failed to read CFG {}", tid, input);
+    }
+}
+
+fn read_dot_files(path: String) -> Vec<String> {
+    let path_p = Path::new(&path);
+    if !path_p.is_dir() {
+        log::warn!("Ignoring input path {}.", path);
+        Vec::new()
+    } else {
+        match std::fs::read_dir(&path) {
+            Ok(read) => read
+                .into_iter()
+                .flatten()
+                .filter(|x| x.path().extension().unwrap_or_default() == "dot")
+                .map(|x| (path_p.join(x.file_name()).to_str().unwrap().to_string()))
+                .collect::<Vec<_>>(),
+            Err(err) => {
+                log::error!(
+                    "Error while reading folder {}. {}. Path ignored.",
+                    path,
+                    err
+                );
+                Vec::new()
+            }
+        }
     }
 }
