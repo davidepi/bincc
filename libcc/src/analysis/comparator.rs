davidepi/bincc

commit 7414ae11fb47c92962208c78cdd0b9d2749e3f5a

refactor: BasicBlock definition
Now using the BB length instead of the last instruction offset.
Comparator and CFG extractor to fix.

libcc/src/analysis/comparator.rs
@@ -119,11 +119,10 @@ fn remove_overlapping(mut clone_list: Vec<ClonePair>) -> Vec<ClonePair> {
         let mut keep = Vec::with_capacity(clone_list.len());
         while let Some(compare) = clone_list.pop() {
             if !removed.contains(&compare) {
+                // FIXME: This is wrong now that the ending offset has been removed
                 if current.depth() != compare.depth()
-                    && current.a.starting_offset() <= compare.a.starting_offset()
-                    && current.b.starting_offset() <= compare.b.starting_offset()
-                    && current.a.ending_offset() >= compare.a.ending_offset()
-                    && current.b.ending_offset() >= compare.b.ending_offset()
+                    && current.a.offset() <= compare.a.offset()
+                    && current.b.offset() <= compare.b.offset()
                 {
                     // not same depth (otherwise I remove myself),
                     // and one is contained inside the other one
@@ -182,7 +181,7 @@ mod tests {
             Statement::new(0x1, "xor eax, eax"),
             Statement::new(0x2, "jmp 0x1"),
         ];
-        let cfg = CFG::new(&stmts, &ArchX86::new_amd64()).add_sink();
+        let cfg = CFG::new(&stmts, 0x3, &ArchX86::new_amd64()).add_sink();
         let cfs = CFS::new(&cfg);
         assert!(cfs.get_tree().is_none());
         let mut diff = CFSComparator::new(5);
@@ -193,7 +192,7 @@ mod tests {
     #[test]
     fn cloned_full() {
         let stmts = create_function();
-        let cfg = CFG::new(&stmts, &ArchX86::new_amd64()).add_sink();
+        let cfg = CFG::new(&stmts, 0x6C, &ArchX86::new_amd64()).add_sink();
         let cfs = CFS::new(&cfg);
         let mut diff = CFSComparator::new(5);
         let first = diff.insert(&cfs, "first");
@@ -204,8 +203,7 @@ mod tests {
         let s = second.unwrap();
         assert_eq!(s.len(), 1);
         let pair = s.last().unwrap();
-        assert_eq!(pair.first_tree().starting_offset(), 0);
-        assert_eq!(pair.first_tree().ending_offset(), SINK_ADDR);
+        assert_eq!(pair.first_tree().offset(), 0);
         assert_eq!(s[0].first_name(), "first");
         assert_eq!(s[0].second_name(), "other");
     }
@@ -213,7 +211,7 @@ mod tests {
     #[test]
     fn cloned_partial() {
         let mut stmts = create_function();
-        let cfg0 = CFG::new(&stmts, &ArchX86::new_amd64()).add_sink();
+        let cfg0 = CFG::new(&stmts, 0x6C, &ArchX86::new_amd64()).add_sink();
         let cfs0 = CFS::new(&cfg0);
         let mut diff = CFSComparator::new(2);
         let first = diff.insert(&cfs0, "first");
@@ -225,7 +223,7 @@ mod tests {
         stmts[10] = Statement::new(0x28, "nop");
         stmts[11] = Statement::new(0x2C, "nop");
         stmts[12] = Statement::new(0x30, "nop");
-        let cfg1 = CFG::new(&stmts, &ArchX86::new_amd64()).add_sink();
+        let cfg1 = CFG::new(&stmts, 0x34, &ArchX86::new_amd64()).add_sink();
         let cfs1 = CFS::new(&cfg1);
         let second = diff.insert(&cfs1, "second");
         assert!(second.is_some());

libcc/src/analysis/blocks.rs
@@ -37,8 +37,7 @@ impl Display for BlockType {
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct NestedBlock {
-    pub(crate) first: u64,
-    pub(crate) last: u64,
+    pub(crate) offset: u64,
     pub(crate) block_type: BlockType,
     pub(crate) content: Vec<StructureBlock>,
     pub(crate) depth: u32,
@@ -47,15 +46,11 @@ pub struct NestedBlock {
 impl NestedBlock {
     pub fn new(bt: BlockType, children: Vec<StructureBlock>) -> NestedBlock {
         let old_depth = children.iter().fold(0, |max, val| max.max(val.depth()));
-        let lower_bound = children
+        let offset = children
             .iter()
-            .fold(u64::MAX, |min, val| min.min(val.starting_offset()));
-        let upper_bound = children
-            .iter()
-            .fold(0, |max, val| max.max(val.ending_offset()));
+            .fold(u64::MAX, |min, val| min.min(val.offset()));
         NestedBlock {
-            first: lower_bound,
-            last: upper_bound,
+            offset,
             block_type: bt,
             content: children,
             depth: old_depth + 1,
@@ -149,17 +144,10 @@ impl StructureBlock {
         }
     }
 
-    pub fn starting_offset(&self) -> u64 {
+    pub fn offset(&self) -> u64 {
         match self {
-            StructureBlock::Basic(bb) => bb.first,
-            StructureBlock::Nested(nb) => nb.first,
-        }
-    }
-
-    pub fn ending_offset(&self) -> u64 {
-        match self {
-            StructureBlock::Basic(bb) => bb.last,
-            StructureBlock::Nested(nb) => nb.last,
+            StructureBlock::Basic(bb) => bb.offset,
+            StructureBlock::Nested(nb) => nb.offset,
         }
     }
 }
@@ -199,8 +187,8 @@ mod tests {
         // checks that despite having two different StructureBlock, if their content is the same Rc
         // equality returns true.
         let bb = Rc::new(BasicBlock {
-            first: 0,
-            last: 0xA,
+            offset: 1,
+            length: 0xA,
         });
         let sb0 = StructureBlock::from(bb.clone());
         let sb1 = StructureBlock::from(bb);
@@ -211,12 +199,12 @@ mod tests {
     fn structure_block_structural_equality() {
         // two basic blocks with different content, but structural equality should be the same
         let bb0 = Rc::new(BasicBlock {
-            first: 0,
-            last: 0xA,
+            offset: 1,
+            length: 0xA,
         });
         let bb1 = Rc::new(BasicBlock {
-            first: 0,
-            last: 0xB,
+            offset: 1,
+            length: 0xB,
         });
         let sb0 = StructureBlock::from(bb0);
         let sb1 = StructureBlock::from(bb1);
@@ -227,12 +215,12 @@ mod tests {
     #[test]
     fn structural_hash_different_id() {
         let bb0 = StructureBlock::from(Rc::new(BasicBlock {
-            first: 0,
-            last: 0xA,
+            offset: 1,
+            length: 0xA,
         }));
         let bb1 = StructureBlock::from(Rc::new(BasicBlock {
-            first: 0xA,
-            last: 0xC,
+            offset: 0xA,
+            length: 0xC,
         }));
         let hashes = calculate_hashes(bb0, bb1);
         assert_eq!(hashes.0, hashes.1)
@@ -240,7 +228,10 @@ mod tests {
 
     #[test]
     fn structural_hash_same_order() {
-        let bb = StructureBlock::from(Rc::new(BasicBlock { first: 0, last: 0 }));
+        let bb = StructureBlock::from(Rc::new(BasicBlock {
+            offset: 1,
+            length: 1,
+        }));
         let self_loop = StructureBlock::from(Rc::new(NestedBlock::new(
             BlockType::SelfLooping,
             vec![bb.clone()],
@@ -263,7 +254,10 @@ mod tests {
 
     #[test]
     fn structural_equality_same_order() {
-        let bb = StructureBlock::from(Rc::new(BasicBlock { first: 0, last: 0 }));
+        let bb = StructureBlock::from(Rc::new(BasicBlock {
+            offset: 1,
+            length: 1,
+        }));
         let self_loop = StructureBlock::from(Rc::new(NestedBlock::new(
             BlockType::SelfLooping,
             vec![bb.clone()],
@@ -285,7 +279,10 @@ mod tests {
 
     #[test]
     fn structural_hash_different_order() {
-        let bb = StructureBlock::from(Rc::new(BasicBlock { first: 0, last: 0 }));
+        let bb = StructureBlock::from(Rc::new(BasicBlock {
+            offset: 1,
+            length: 1,
+        }));
         let self_loop = StructureBlock::from(Rc::new(NestedBlock::new(
             BlockType::SelfLooping,
             vec![bb.clone()],
@@ -308,7 +305,10 @@ mod tests {
 
     #[test]
     fn structural_equality_different_order() {
-        let bb = StructureBlock::from(Rc::new(BasicBlock { first: 0, last: 0 }));
+        let bb = StructureBlock::from(Rc::new(BasicBlock {
+            offset: 1,
+            length: 1,
+        }));
         let self_loop = StructureBlock::from(Rc::new(NestedBlock::new(
             BlockType::SelfLooping,
             vec![bb.clone()],

libcc/src/analysis/cfs.rs
@@ -73,7 +73,7 @@ impl CFS {
         let mut dot = "digraph {\n".to_string();
         let mut stack = self.get_tree().iter().cloned().collect::<Vec<_>>();
         while let Some(node) = stack.pop() {
-            let node_id = node.starting_offset();
+            let node_id = node.offset();
             match node {
                 StructureBlock::Basic(_) => writeln!(
                     dot,
@@ -87,7 +87,7 @@ impl CFS {
             }
             .unwrap();
             for child in node.children().iter().cloned() {
-                let child_id = child.starting_offset();
+                let child_id = child.offset();
                 writeln!(dot, "{}->{}", node_id, child_id).unwrap();
                 stack.push(child);
             }
@@ -108,7 +108,7 @@ fn print_subgraph<T: std::fmt::Write>(node: &StructureBlock, id: usize, fmt: &mu
     match node {
         StructureBlock::Basic(bb) => {
             if !bb.is_entry_point() && !bb.is_sink() {
-                writeln!(fmt, "{};", bb.first).unwrap();
+                writeln!(fmt, "{};", bb.offset).unwrap();
             }
         }
         StructureBlock::Nested(_) => {
@@ -660,7 +660,7 @@ fn reduce_proper_interval<'a>(
                     let mut iter = children_union.iter().copied();
                     left = iter.next().unwrap();
                     right = iter.next().unwrap();
-                    if left.starting_offset() > right.starting_offset() {
+                    if left.offset() > right.offset() {
                         // in this case choosing left and right may be nondeterministic. So left
                         // is always the one with the lowest offset.
                         swap(&mut left, &mut right)
@@ -991,8 +991,8 @@ fn denaturate_loop(
                     match depth_map.get(a).cmp(&depth_map.get(b)) {
                         Ordering::Less => b,
                         Ordering::Equal => {
-                            let difference_a = distance(node.first, a.first);
-                            let difference_b = distance(node.first, b.first);
+                            let difference_a = distance(node.offset, a.offset);
+                            let difference_b = distance(node.offset, b.offset);
                             match difference_a.cmp(&difference_b) {
                                 Ordering::Less => a,
                                 Ordering::Equal => {
@@ -1033,7 +1033,7 @@ fn denaturate_loop(
                 //keep the one with further offset
                 let (_, index_max_diff) = exits_vec
                     .iter()
-                    .map(|x| distance(node.first, x.first))
+                    .map(|x| distance(node.offset, x.offset))
                     .enumerate()
                     .map(|(index, value)| (value, index))
                     .max()
@@ -1087,7 +1087,7 @@ mod tests {
             let cap = create_cfg!(@count $($src),*);
             let nodes = (0..)
                         .take(cap)
-                        .map(|x| Rc::new(BasicBlock { first: x, last: x+1 }))
+                        .map(|x| Rc::new(BasicBlock { offset: x, length: 1 }))
                         .collect::<Vec<_>>();
             #[allow(unused_mut)]
             let mut edges = std::collections::HashMap::with_capacity(cap);
@@ -1125,7 +1125,7 @@ mod tests {
         };
         let depth = cfs::calculate_depth(&cfg);
         let mut visit = cfg.dfs_postorder().collect::<Vec<_>>();
-        visit.sort_by(|a, b| a.first.cmp(&b.first));
+        visit.sort_by(|a, b| a.offset.cmp(&b.offset));
         let expected = vec![4_usize, 1, 3, 2, 0, 1, 0];
         let actual = visit
             .into_iter()

libcc/src/disasm/disassembler.rs
@@ -13,7 +13,7 @@ pub struct BareCFG {
     pub root: Option<u64>,
     /// Vector of basic blocks. Each tuple contains a basic block in the form:
     /// - offset of the first instruction.
-    /// - offset of the last instruction.
+    /// - length of the basic block.
     pub blocks: Vec<(u64, u64)>,
     /// Vector of CFG edges. Each tuple contains an edge in the form:
     /// - offset of the source basic block.

libcc/src/disasm/radare2.rs
@@ -454,7 +454,7 @@ mod tests {
         let cfg = cfg.unwrap();
         let expected = BareCFG {
             root: Some(0x1000),
-            blocks: vec![(0x1000, 0x1012), (0x1014, 0x1014), (0x1016, 0x101A)],
+            blocks: vec![(0x1000, 20), (0x1014, 2), (0x1016, 5)],
             edges: vec![(0x1000, 0x1016), (0x1000, 0x1014), (0x1014, 0x1016)],
         };
         assert_eq!(cfg, expected);

commit b8b48b2c8db1fb04ec7a71721d3dfc17f3f91fcc

fix: remove overlapping comparisong fixed
Broken since 7414ae1

libcc/src/analysis/comparator.rs
@@ -1,5 +1,4 @@
 use crate::analysis::blocks::StructureBlock;
-use crate::analysis::CFS;
 use fnv::FnvHashMap;
 use std::collections::hash_map::DefaultHasher;
 use std::collections::{HashMap, HashSet};
@@ -67,41 +66,41 @@ impl CFSComparator {
         }
     }
 
-    pub fn insert(&mut self, other: &CFS, identifier: &str) -> Option<Vec<ClonePair>> {
-        if let Some(root) = other.get_tree() {
-            let mut stack = vec![root];
-            let mut ret = Vec::new();
-            while let Some(node) = stack.pop() {
-                if node.depth() >= self.mindepth {
-                    let mut hasher = DefaultHasher::new();
-                    node.structural_hash(&mut hasher);
-                    let hash = hasher.finish();
-                    if let Some(original) = self.hashes.get(&hash) {
-                        if original.structural_equality(&node) {
-                            // pick only a_name: b is never inserted in the hashmap because it's the
-                            // clone! so its name is used only in this pair and never recorded
-                            let a_name = self.names.get(original).unwrap().as_str();
-                            let pair =
-                                ClonePair::new(original.clone(), a_name, node.clone(), identifier);
-                            ret.push(pair);
-                        } else {
-                            log::warn!("Same structural hash but different structure.");
-                        }
+    pub fn compare_and_insert(
+        &mut self,
+        other: &StructureBlock,
+        identifier: &str,
+    ) -> Option<Vec<ClonePair>> {
+        let mut stack = vec![other.clone()];
+        let mut ret = Vec::new();
+        while let Some(node) = stack.pop() {
+            if node.depth() >= self.mindepth {
+                let mut hasher = DefaultHasher::new();
+                node.structural_hash(&mut hasher);
+                let hash = hasher.finish();
+                if let Some(original) = self.hashes.get(&hash) {
+                    if original.structural_equality(&node) {
+                        // pick only a_name: b is never inserted in the hashmap because it's the
+                        // clone! so its name is used only in this pair and never recorded
+                        let a_name = self.names.get(original).unwrap().as_str();
+                        let pair =
+                            ClonePair::new(original.clone(), a_name, node.clone(), identifier);
+                        ret.push(pair);
                     } else {
-                        self.hashes.insert(hash, node.clone());
-                        self.names.insert(node.clone(), identifier.to_string());
+                        log::warn!("Same structural hash but different structure.");
                     }
-                    let mut children = node.children().to_vec();
-                    stack.append(&mut children)
+                } else {
+                    self.hashes.insert(hash, node.clone());
+                    self.names.insert(node.clone(), identifier.to_string());
                 }
+                let mut children = node.children().to_vec();
+                stack.append(&mut children)
             }
-            if !ret.is_empty() {
-                ret = remove_overlapping(ret);
-            }
-            Some(ret)
-        } else {
-            None
         }
+        if !ret.is_empty() {
+            ret = remove_overlapping(ret);
+        }
+        Some(ret)
     }
 }
 
@@ -119,11 +118,7 @@ fn remove_overlapping(mut clone_list: Vec<ClonePair>) -> Vec<ClonePair> {
         let mut keep = Vec::with_capacity(clone_list.len());
         while let Some(compare) = clone_list.pop() {
             if !removed.contains(&compare) {
-                // FIXME: This is wrong now that the ending offset has been removed
-                if current.depth() != compare.depth()
-                    && current.a.offset() <= compare.a.offset()
-                    && current.b.offset() <= compare.b.offset()
-                {
+                if current.depth() != compare.depth() && overlaps(&current, &compare) {
                     // not same depth (otherwise I remove myself),
                     // and one is contained inside the other one
                     removed.insert(compare);
@@ -135,6 +130,38 @@ fn remove_overlapping(mut clone_list: Vec<ClonePair>) -> Vec<ClonePair> {
         clone_list = keep;
     }
     clone_list
+}
+
+fn overlaps(a: &ClonePair, b: &ClonePair) -> bool {
+    let mut retval = false;
+    if a.depth() >= b.depth()
+        && a.first_name() == b.first_name()
+        && a.second_name() == b.second_name()
+    {
+        let mut first_ok = false;
+        let mut second_ok = false;
+        let mut stack = a.a.children().iter().collect::<Vec<_>>();
+        while let Some(child) = stack.pop() {
+            if child.offset() == b.a.offset() {
+                first_ok = true;
+                break;
+            } else {
+                stack.extend(child.children());
+            }
+        }
+        if first_ok {
+            stack.clear();
+            stack.extend(a.b.children());
+            while let Some(child) = stack.pop() {
+                if child.offset() == b.b.offset() {
+                    second_ok = true;
+                    break;
+                }
+            }
+        }
+        retval = first_ok && second_ok;
+    }
+    retval
 }
 
 #[cfg(test)]
@@ -175,30 +202,15 @@ mod tests {
     }
 
     #[test]
-    fn cloned_failed_cfs() {
-        let stmts = vec![
-            Statement::new(0x0, "jge 0x2"),
-            Statement::new(0x1, "xor eax, eax"),
-            Statement::new(0x2, "jmp 0x1"),
-        ];
-        let cfg = CFG::new(&stmts, 0x3, &ArchX86::new_amd64()).add_sink();
-        let cfs = CFS::new(&cfg);
-        assert!(cfs.get_tree().is_none());
-        let mut diff = CFSComparator::new(5);
-        let first = diff.insert(&cfs, "failed");
-        assert!(first.is_none());
-    }
-
-    #[test]
     fn cloned_full() {
         let stmts = create_function();
         let cfg = CFG::new(&stmts, 0x6C, &ArchX86::new_amd64()).add_sink();
         let cfs = CFS::new(&cfg);
         let mut diff = CFSComparator::new(5);
-        let first = diff.insert(&cfs, "first");
+        let first = diff.compare_and_insert(&cfs.get_tree().unwrap(), "first");
         assert!(first.is_some());
         assert!(first.unwrap().is_empty());
-        let second = diff.insert(&cfs, "other");
+        let second = diff.compare_and_insert(&cfs.get_tree().unwrap(), "other");
         assert!(second.is_some());
         let s = second.unwrap();
         assert_eq!(s.len(), 1);
@@ -214,7 +226,7 @@ mod tests {
         let cfg0 = CFG::new(&stmts, 0x6C, &ArchX86::new_amd64()).add_sink();
         let cfs0 = CFS::new(&cfg0);
         let mut diff = CFSComparator::new(2);
-        let first = diff.insert(&cfs0, "first");
+        let first = diff.compare_and_insert(&cfs0.get_tree().unwrap(), "first");
         assert!(first.is_some());
         assert!(first.unwrap().is_empty());
         stmts = create_function();
@@ -223,9 +235,9 @@ mod tests {
         stmts[10] = Statement::new(0x28, "nop");
         stmts[11] = Statement::new(0x2C, "nop");
         stmts[12] = Statement::new(0x30, "nop");
-        let cfg1 = CFG::new(&stmts, 0x34, &ArchX86::new_amd64()).add_sink();
+        let cfg1 = CFG::new(&stmts, 0x6C, &ArchX86::new_amd64()).add_sink();
         let cfs1 = CFS::new(&cfg1);
-        let second = diff.insert(&cfs1, "second");
+        let second = diff.compare_and_insert(&cfs1.get_tree().unwrap(), "second");
         assert!(second.is_some());
         let s = second.unwrap();
         assert_eq!(s.len(), 2);